21e24fe9e350ea971b2e5e04e6b87e1e890cc00e
Sky Xu <skyrocks88xmz@gmail.com>
2017-04-01 17:52:57 -0400

    finish frontend functionality and added modal

--- src/components/Savedfood/Savedfood.js
+++ src/components/Savedfood/Savedfood.js
@@ -5,7 +5,8 @@ import update from "react-addons-update";
 import SavedFoodInfo from './SavedFoodInfo';
 
 let caloriesTotal = 0;
-
+let caloriesBurnedTotal = 0;
+let cravingCal = 650;
 class Savedfood extends Component {
   constructor(props) {
     super(props);
@@ -21,9 +22,22 @@ class Savedfood extends Component {
       height_cm: 190,
       ran_hours: 1,
       ran_miles: 3,
-      weight_kg: 60
+      weight_kg: 60,
+      yoga_min: 30,
+      walk_miles: 1,
+      walk_hours: 1,
+      spin_min: 30
     },
-    burnedCalories: ''
+    burnedCalories: '',
+    isVisible: {
+      display: 'none'
+    },
+    modalShow: {
+      display: 'none'
+    },
+    modalStop: {
+      display: 'none'
+    }
 
     }
   }
@@ -35,7 +49,6 @@ class Savedfood extends Component {
     })
     .then((results) => {
       results.json().then((data) => {
-       // console.log("&&&&&&&",data.data)
         this.setState({
           food: data.data,
         });
@@ -57,7 +70,6 @@ class Savedfood extends Component {
        }
     })
     this.setState(newState);
-    console.log("HANdle Change )))))", this.state)
   }
   searchBurnedCarlories(event) {
     console.log('%%%%%',this.state);
@@ -73,12 +85,17 @@ class Savedfood extends Component {
         gender: this.state.burnFat.gender,
         weight_kg: parseInt(this.state.burnFat.weight_kg),
         height_cm: parseInt(this.state.burnFat.height_cm),
-        age: parseInt(this.state.burnFat.age)
+        age: parseInt(this.state.burnFat.age),
+        yoga_min: parseInt(this.state.burnFat.yoga_min),
+        walk_miles: parseInt(this.state.burnFat.walk_miles),
+        walk_hours: parseInt(this.state.burnFat.walk_hours),
+        spin_min: parseInt(this.state.burnFat.spin_min)
       })
     })
      .then((results) => {
       results.json().then((data) => {
-        this.setState({burnedCalories: data.exercises[0].nf_calories})
+        caloriesBurnedTotal = data.exercises[0].nf_calories+data.exercises[1].nf_calories+data.exercises[2].nf_calories+data.exercises[3].nf_calories
+        this.setState({burnedCalories: caloriesBurnedTotal})
         console.log("THIS IS searchBurnedCarlories",data)
       })
       .then(()=>{
@@ -90,9 +107,34 @@ class Savedfood extends Component {
     })
   }
 
+  calculateFoodCalories(event){
+     this.setState({ isVisible: {display: 'block'} })
+  }
+
+  // compare the craving list cal and burnedCal
+  compareCalories(event){
+    if (cravingCal < this.state.burnedCalories) {
+      this.setState({ modalShow: {display: 'block'} })
+    } else {
+      this.setState({ modalStop: {display: 'block'} });
+    }
+  }
+
+   // let addAnimation(()=>{
+   //  let animate =  document.getElementsByClassName("warn")
+   //  animate.className += "animated bounceOutLeft"
+   // })
+
+
   render() {
    return(
     <div className="savedfood-container">
+    <div className="stop-filter" style={this.state.modalStop}>
+      <h1 className="warn">Stop eating, you fat ass!</h1>
+    </div>
+    <div className="filter" style={this.state.modalShow}>
+      <h1 className="congrats">Congrats!</h1><h1 className="enjoy">Enjoy</h1><h1 className="craving">your cravings</h1>
+    </div>
      <div className="exercise">
       <h2>Exercise</h2>
        <form
@@ -128,16 +170,37 @@ class Savedfood extends Component {
        onChange={this.handleChange.bind(this)}
        placeholder="type in weight"
        />
+       <input type="text"
+       name="yoga_min"
+       onChange={this.handleChange.bind(this)}
+       placeholder="type in yoga min"
+       />
+       <input type="text"
+       name="walk_miles"
+       onChange={this.handleChange.bind(this)}
+       placeholder="type in walk mile"
+       />
+       <input type="text"
+       name="walk_hours"
+       onChange={this.handleChange.bind(this)}
+       placeholder="type in walk hour"
+       />
+       <input type="text"
+       name="spin_min"
+       onChange={this.handleChange.bind(this)}
+       placeholder="type in spin min"
+       />
        <input type="submit" value="Search calories" />
       </form>
      {this.state.burnedCalories}
      </div>
 
-     <div className="craving_list">
+  <div className="craving_list">
      <h2>Your Craving food list</h2>
      {this.state.food.map((food) => {
       console.log("THIS IS FOOD IN MAP",food)
       caloriesTotal += food.calories;
+       console.log("THIS IS FOOD Cal",caloriesTotal)
       return(
       <div>
        <div key={food.id}>
@@ -150,10 +213,20 @@ class Savedfood extends Component {
       </div>
       )
       })}
-     <h3>{console.log(caloriesTotal)}</h3>
-     </div>
-
+       <button className="submit_foodcal" onClick={this.calculateFoodCalories.bind(this)}>
+       find out craving list calories!
+       </button>
+        <div style={this.state.isVisible}>
+          <h3>{cravingCal}</h3>
+        </div>
     </div>
+         <div className="see_if_can_eat">
+          <button onClick={this.compareCalories.bind(this)}>
+          find out if can eat
+          </button>
+         </div>
+  </div>
+
      )
   }
 
